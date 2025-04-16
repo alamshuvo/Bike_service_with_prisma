79cfdee680d92e3ccf292ac9a5e06d16d3340aa6
alamshuvo <iftakharalamshuvo11@gmail.com>
2025-04-16 22:06:54 +0600

    bike service backed init

---
+++ src/app.ts
@@ -0,0 +1,21 @@
+import express, { Application, NextFunction, Request, Response } from 'express';
+import cors from 'cors';
+
+import cookieParser from 'cookie-parser'
+const app:Application = express();
+
+app.use(cors());
+app.use(cookieParser())
+//parser
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+app.get('/',(req:Request,res:Response)=>{
+    res.send({
+        Message:"Health care Server..."
+    })
+})
+// app.use('/api/v1',router);
+// app.use(globalErrorHandler);
+
+// app.use(notFound)
+export default app ;
