a46a5011db4b204d88ca0c80ae68de23d603b473
alamshuvo <iftakharalamshuvo11@gmail.com>
2025-04-16 22:40:42 +0600

    prisma schema added

--- src/app.ts
+++ src/app.ts
@@ -11,7 +11,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.get('/',(req:Request,res:Response)=>{
     res.send({
-        Message:"Health care Server..."
+        Message:"Bike Server  Server..."
     })
 })
 // app.use('/api/v1',router);
